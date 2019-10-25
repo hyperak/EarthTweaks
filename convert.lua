bc783d255bd389f89d1a8b91506dd2cf66f60101
Diego Esaá <diegitxpokemon@gmail.com>
2019-10-25 17:01:13 -0400

    Update convert.lua

--- convert.lua
+++ convert.lua
@@ -11,17 +11,17 @@
 local fs = {}
 
 function fs.length(filename)
-  local fh = assert(io.open(filename, "rb"))
-  local len = assert(fh:seek("end"))
-  fh:close()
-  return len
+   local fh = assert(io.open(filename, "rb"))
+   local len = assert(fh:seek("end"))
+   fh:close()
+   return len
 end
 
 -- Return true if file exists and is readable.
 function fs.exists(path)
-  local file = io.open(path, "rb")
-  if file then file:close() end
-  return file ~= nil
+   local file = io.open(path, "rb")
+   if file then file:close() end
+   return file ~= nil
 end
 
 -- Guarded seek.
@@ -29,30 +29,32 @@ end
 -- on error.
 -- Requires Lua 5.1.
 function seek(fh, ...)
-  assert(fh:seek(...))
+   assert(fh:seek(...))
 end
 
 -- Read an entire file.
-function fs.read(filename)
-  local fh = assert(io.open(filename, "rb"))
-  local contents = assert(fh:read("*a")) -- "a" in Lua 5.3; "*a" in Lua 5.1 and 5.2
-  fh:close()
-  return contents
+function fs.readall(filename)
+   LOG("running filesystem read")
+   local fh = assert(io.open(filename, "rb"))
+   LOG(type(fh))
+   local contents = assert(fh:read("*a")) -- "a" in Lua 5.3; "*a" in Lua 5.1 and 5.2
+   fh:close()
+   return contents
 end
 
 -- Write a string to a file.
 function fs.write(filename, contents)
-  local fh = assert(io.open(filename, "wb"))
-  fh:write(contents)
-  fh:flush()
-  fh:close()
+   local fh = assert(io.open(filename, "wb"))
+   fh:write(contents)
+   fh:flush()
+   fh:close()
 end
 
 -- Read, process file contents, write.
 function fs.modify(filename, modify_func)
-  local contents = readall(filename)
-  contents = modify_func(contents)
-  write(filename, contents)
+   local contents = readall(filename)
+   contents = modify_func(contents)
+   write(filename, contents)
 end
 
 LOG("Loaded file operations")
@@ -145,7 +147,7 @@ function getPlaceFromCoords(lat, lon)
 	       end
 	    end
    end
-   local file_r = fs.read('places.json')
+   local file_r = fs.readall('places.json')
    LOG(file_r)
    local j_file_r = {}
    if type(file_r) == 'string' then
